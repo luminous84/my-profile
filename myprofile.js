7b98876887ba1dfb195a7d0d487ae4532a6831a1
luminous84 <51385241+luminous84@users.noreply.github.com>
2019-11-01 16:05:21 -0400

    Add files via upload

---
+++ myprofile.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+
+
+$('.button1-left').click(function(){
+  $('.description1-noDisplay').toggle();
+  });
+
+$('.button2-left').click(function(){
+  $('.description2-noDisplay').toggle();
+  });
+
+$('.button3-left').click(function(){
+  $('.description3-noDisplay').toggle();
+  });
+
+$('.button4-left').click(function(){
+  $('.description4-noDisplay').toggle();
+  });
+
+$('.button5-left').click(function(){
+  $('.description5-noDisplay').toggle();
+  });
+
+$('.button6-left').click(function(){
+  $('.description6-noDisplay').toggle();
+  });
+
+
+});
